f6f3d7a6ab9deb552e9da1a7960a11a7f4fe2acc
zhaowei.zhang01 <zhaowei.zhang01@hand-china.com>
2019-08-15 16:24:28 +0800

    微信SDK获取userid成功+mybatis-plus改造成功

---
+++ src/main/java/com/zzz/springteam21922/domain/WxModel.java
@@ -0,0 +1,32 @@
+package com.zzz.springteam21922.domain;
+
+public class WxModel {
+	private String token;
+	private Integer expires_in;
+	private String token_begin_str;
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public Integer getExpires_in() {
+		return expires_in;
+	}
+	public void setExpires_in(Integer expires_in) {
+		this.expires_in = expires_in;
+	}
+	public String getToken_begin_str() {
+		return token_begin_str;
+	}
+	public void setToken_begin_str(String token_begin_str) {
+		this.token_begin_str = token_begin_str;
+	}
+	@Override
+	public String toString() {
+		return "WxModel [token=" + token + ", expires_in=" + expires_in
+				+ ", token_begin_str=" + token_begin_str + "]";
+	}
+	
+
+}
